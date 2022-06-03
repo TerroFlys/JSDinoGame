3123997dfe3ab8da7b0fe66eebbf8fa97b2ef72c
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-06-03 14:53:46 +0200

    Initial commit with starting bootstrap html and to torture the user a loading bar

---
+++ js/load.js
@@ -0,0 +1,24 @@
+const loadingElement = document.getElementById("progress-bar");
+const loadingDivElement = document.getElementById("loading-div");
+const gameElement = document.getElementById("game-canvas");
+let number = 0;
+let increaser = 1;
+let randomIncreaser = 30;
+const increaseLoadingBar = () => {
+    if (number >= 100){
+        loadingDivElement.style.display = "none";
+        gameElement.style.display = "";
+
+    }
+    if (loadingDivElement.style.display === "none") return;
+    number += increaser;
+    increaser += randomIncreaser;
+    loadingElement.style.width = number+"%";
+
+}
+
+const loading = () => {
+    setInterval(increaseLoadingBar, 777);
+}
+
+loading();
