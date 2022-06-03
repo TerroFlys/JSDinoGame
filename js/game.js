c297ab8b326a402c7a8430efd19a2fdbf4988aae
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-06-03 15:26:23 +0200

    Added gameloop, drawplayer, clearscreen functions

--- js/game.js
+++ js/game.js
@@ -9,5 +9,26 @@ const ctx = canvas.getContext("2d");
 const player = {
     x: 0,
     y: 0,
-    size: 30
+    xSize: 20,
+    ySize: 60
 }
+//gameLoop
+const drawGame = () => {
+    requestAnimationFrame(drawGame)
+    clearScreen()
+    drawPlayer()
+}
+//method to clear the screen in order to redraw
+const clearScreen = () => {
+    ctx.fillStyle = "black"
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+//draw the player
+const drawPlayer = () => {
+    ctx.fillStyle = "white"
+    ctx.fillRect(player.x,player.y,player.xSize,player.ySize)
+}
+
+//start the gameloop
+drawGame();
