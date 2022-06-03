433340b1d60af2d09143976ad724c1be78aa5216
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-06-03 15:49:08 +0200

    More gamelogic (player jumping)

--- js/game.js
+++ js/game.js
@@ -7,15 +7,34 @@ const ctx = canvas.getContext("2d");
 
 //player object
 const player = {
-    x: 0,
-    y: 0,
+    x: 0, // should not be changed
+    y: 0, // This will be changed when jumping
     xSize: 20,
-    ySize: 60
+    ySize: 40
 }
+//variables
+let score = 0;
+let highscore = 0;
+let isJumping = false; // change this to true to start the jump
+let isFalling = true; // when reaching the ground this should be changed to false
+
+const baseJumpForce = 0.6;
+let jumpforce = baseJumpForce;
+const jumpIncreaser = 1.08;
+
+const baseFallForce = 0.2;
+let fallForce = baseFallForce;
+const fallIncreaser = 1.04;
+
 //gameLoop
 const drawGame = () => {
     requestAnimationFrame(drawGame)
+    //clear the screen
     clearScreen()
+    //then do the 'physics' and 'checks'
+    jump()
+    fall()
+    //after that draw everything
     drawPlayer()
 }
 //method to clear the screen in order to redraw
@@ -29,6 +48,37 @@ const drawPlayer = () => {
     ctx.fillStyle = "white"
     ctx.fillRect(player.x,player.y,player.xSize,player.ySize)
 }
+
+//jump method
+const jump = () => {
+    if (!isJumping || isFalling) return;
+    //check if jump reached jump height to turn isJumping off and turn isFalling on
+    player.y -= jumpforce;
+    jumpforce *= jumpIncreaser;
+    if (player.y <= 0){ // reaching max height
+        jumpforce = baseJumpForce;
+        isJumping = false
+        isFalling = true
+    }
+
+}
+const fall = () => {
+    if (isJumping || !isFalling) return;
+    player.y += fallForce;
+    fallForce *= fallIncreaser;
+    if (player.y >= canvas.height-player.ySize){
+        fallForce = baseFallForce;
+        isFalling = false
+    };
+}
+
+const keyDown = (event) => {
+    if (event.keyCode !== 32 || isJumping || isFalling) return; //keyCode 32 = space
+    isJumping = true; // put this on true after passing the IF statement
+
+}
+//event listener for space keypress
+document.body.addEventListener('keydown', keyDown)
 
 //start the gameloop
 drawGame();
